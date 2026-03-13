4e293722767547880ba404d0c45df9196caf8608
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-13 18:04:48 +0900

    푸시

---
+++ Chunk/IEumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+static class IEnumerableExtensions {
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> enumerable, int size) where T : new() {
+        if (size < 1) {
+            throw new ArgumentException();
+        }
+
+        // enumerable을 size개씩 분해.
+        List<T> chunk = new List<T>();
+
+        List<T> tempVal = new List<T>();
+        foreach (var item in enumerable) {
+            if (chunk.Count == size) {
+                yield return tempVal;
+                tempVal = new List<T>();
+            } else {
+                tempVal.Add(item);
+            }
+        }
+    }
+}
--- Chunk/Program.cs
+++ Chunk/Program.cs
@@ -2,5 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine($"=== 컬렉션 청킹 테스트 ===");
+Console.WriteLine($"[숫자를 3개씩 그룹화]");
+List<int> ints = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+var chunks = ints.Chunk(3);
+
+foreach ( IEnumerable<int> chunk in chunks ) {
+    Console.WriteLine($"[");
+    Console.WriteLine(string.Join(", ", chunk));
+    Console.WriteLine($"]");
+}
