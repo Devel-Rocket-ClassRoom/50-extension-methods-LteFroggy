40df0fb020ec925d6182794b618169bed6ed684b
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-16 00:17:18 +0900

    Shuffle 완료

---
+++ Shuffle/ListExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+static class ListExtension {
+    public static void Shuffle<T> (this IList<T> list) {
+        Random rand = new Random();
+
+        for (int i = list.Count - 1; i > 0; i--) {
+            int loc = rand.Next(i - 1);
+            T tmp = list[i];
+            list[i] = list[loc];
+            list[loc] = tmp;
+        }
+    }
+}
--- Shuffle/Program.cs
+++ Shuffle/Program.cs
@@ -1,5 +1,27 @@
 using System;
 using System.Collections.Generic;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+List<int> intList = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+List<string> cardList = new List<string>(new string[] {"♠A", "♥K", "♦Q", "♣J"});
+List<string> studentList = new List<string>(new string[] { "김철수", "이영희", "박민수", "최지연", "정우진" });
+
+Console.WriteLine($"=== 컬렉션 셔플 테스트 ===");
+Console.WriteLine();
+
+Console.WriteLine($"[숫자 리스트 셔플]");
+Console.WriteLine($"원본 : {string.Join(", ", intList)}");
+intList.Shuffle();
+Console.WriteLine($"셔플 : {string.Join(", ", intList)}");
+Console.WriteLine();
+
+Console.WriteLine($"[카드 덱 셔플]");
+Console.WriteLine($"원본 : {string.Join(", ", cardList)}");
+cardList.Shuffle();
+Console.WriteLine($"셔플 : {string.Join(", ", cardList)}");
+Console.WriteLine();
+
+Console.WriteLine($"[학생 순서 무작위 배치 셔플]");
+Console.WriteLine($"원본 : {string.Join(", ", studentList)}");
+studentList.Shuffle();
+Console.WriteLine($"셔플 : {string.Join(", ", studentList)}");
+Console.WriteLine();
