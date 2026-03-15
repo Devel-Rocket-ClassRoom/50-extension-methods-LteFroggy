34f7852648a9180365346b9f5040c57a53038605
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-16 00:16:53 +0900

    Chunk 완료

--- Chunk/IEumerableExtensions.cs
+++ Chunk/IEumerableExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 static class IEnumerableExtensions {
-    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> enumerable, int size) where T : new() {
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> enumerable, int size) {
         if (size < 1) {
             throw new ArgumentException();
         }
@@ -10,14 +10,14 @@ static class IEnumerableExtensions {
         // enumerable을 size개씩 분해.
         List<T> chunk = new List<T>();
 
-        List<T> tempVal = new List<T>();
         foreach (var item in enumerable) {
             if (chunk.Count == size) {
-                yield return tempVal;
-                tempVal = new List<T>();
+                yield return chunk;
+                chunk = new List<T>(new T[] { item });
             } else {
-                tempVal.Add(item);
+                chunk.Add(item);
             }
         }
+        yield return chunk;
     }
 }
--- Chunk/Program.cs
+++ Chunk/Program.cs
@@ -1,14 +1,37 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 Console.WriteLine($"=== 컬렉션 청킹 테스트 ===");
 Console.WriteLine($"[숫자를 3개씩 그룹화]");
 List<int> ints = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 var chunks = ints.Chunk(3);
 
-foreach ( IEnumerable<int> chunk in chunks ) {
-    Console.WriteLine($"[");
-    Console.WriteLine(string.Join(", ", chunk));
+foreach (IEnumerable<int> chunk in chunks ) {
+    Console.Write($"[");
+    Console.Write(string.Join(", ", chunk));
     Console.WriteLine($"]");
 }
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine($"[학생들을 2명씩 팀 구성]");
+List<string> students = new List<string>(new string[] { "김철수", "이영희", "박민수", "최지연", "정우진" });
+var teams = students.Chunk(2);
+var teamNumber = 1;
+foreach (IEnumerable<string> chunk in teams) {
+    Console.Write($"팀 {teamNumber++} : ");
+    Console.WriteLine(string.Join(", ", chunk));
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine($"[페이지별로 5개씩 나누기]");
+var pages = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 });
+var pageChunks = pages.Chunk(5);
+var pageNumber = 1;
+foreach (IEnumerable<int> chunk in pageChunks) {
+    Console.Write($"페이지 {pageNumber} : ");
+    Console.WriteLine(string.Join(", ", chunk));
+}
+Console.WriteLine();
+Console.WriteLine();
