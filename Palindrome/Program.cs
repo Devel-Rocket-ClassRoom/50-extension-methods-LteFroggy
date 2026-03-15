80f2c240130d7f3cd798f8c23af56c9c0ec61684
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-16 00:17:13 +0900

    Palindrome 완료

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,20 @@
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+string[] strs = new string[] { 
+    "토마토",
+    "level",
+    "기러기",
+    "Level",
+    "A man, a plan, a canal: Panama",
+    "race a car",
+    "hello",
+    "가나다",
+    ""
+};
+
+Console.WriteLine($"=== 회문 판별 테스트 ===");
+foreach (string str in strs) {
+    Console.WriteLine($"\"{str}\" -> {str.IsPalindrome()}");
+}
+Console.WriteLine();
+Console.WriteLine();
---
+++ Palindrome/StringExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+static class StringExtension {
+    public static bool IsPalindrome(this string str) {
+
+        // 공백 혹은 null이면 false 반환
+        if (string.IsNullOrEmpty(str)) { return false; }
+
+        // 전부 lowercase로
+        str = str.ToLower();
+
+        // 문자, 숫자만 비교대상으로
+        string newStr = "";
+        foreach (char c in str) {
+            if (char.IsLetterOrDigit(c)) {
+                newStr += c;
+            }
+        }
+        str = newStr;
+
+        // 공백 전부 제거
+        newStr = "";
+        foreach (char c in str) {
+            if (!char.IsWhiteSpace(c)) {
+                newStr += c;
+            }
+        }
+        str = newStr;
+
+        // 판별 
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right) {
+            if (str[left] != str[right]) { return false; }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
